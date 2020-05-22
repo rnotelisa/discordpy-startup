0cd74e48339f4ea9f94be20e8f7308d820d5b05c
rnotelisa <65759262+rnotelisa@users.noreply.github.com>
2020-05-22 18:35:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,22 +1,21 @@
-from discord.ext import commands
-import os
-import traceback
+$ py -3 -m pip install -U discord.py[voice]
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# インストールした discord.py を読み込む
+import discord
 
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
@@ -26,4 +25,5 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
