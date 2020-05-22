b9852c54d912784bd316279d750b1d27aac5687a
rnotelisa <65759262+rnotelisa@users.noreply.github.com>
2020-05-22 18:10:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,7 @@ token = os.environ['DISCORD_BOT_TOKEN']
 async def on_message(message):
     if message.author.bot:
         return
-    if message.content == /neko:
+    if message.content == '/neko':
         await message.channel.send('にゃーん')
 
 
