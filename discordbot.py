35a34d03cd804856d40ce60c1b9c7fddf7545b79
rnotelisa <65759262+rnotelisa@users.noreply.github.com>
2020-05-22 22:50:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,19 +5,17 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-# 起動時に動作する処理
-@bot.command()
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
 
 @bot.command()
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await ctx.send('にゃーん')
+async def ping(ctx):
+    await ctx.send('pong')
+
 
 bot.run(token)
