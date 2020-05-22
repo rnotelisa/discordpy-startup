aabbb1b158a4a3b1097daa2eb5c052fd8fe5f838
rnotelisa <65759262+rnotelisa@users.noreply.github.com>
2020-05-22 18:12:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,27 @@
-from discord.ext import commands
-import os
-import traceback
+# インストールした discord.py を読み込む
+import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'NzExMjA3NDYxODc2NjYyMzMz.XseFkw.UAvtJ8BoFFMlr8qJLTar2Vcw2m4'
 
-@bot.command()
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
 async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
+    # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
