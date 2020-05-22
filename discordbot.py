ccdab46f06bcc82b8cfcbfd7427b16fa733cd652
rnotelisa <65759262+rnotelisa@users.noreply.github.com>
2020-05-22 22:50:00 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,15 +5,11 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
+# 起動時に動作する処理
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
 @bot.command()
 async def on_message(message):
