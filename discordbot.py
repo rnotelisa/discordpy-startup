63358b6ea60f1db95674062c5d635bacd7644223
rnotelisa <65759262+rnotelisa@users.noreply.github.com>
2020-05-22 18:05:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,11 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 
 bot.run(token)
