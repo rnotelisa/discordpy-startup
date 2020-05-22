b86d6b43d33768a94d200a007c5ae420eff3f1c6
rnotelisa <65759262+rnotelisa@users.noreply.github.com>
2020-05-22 22:44:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,16 +16,12 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
-async def cat(ctx):
-    await ctx.send('にゃ～ん')
-
-@bot.command()
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
-        await message.channel.send('にゃーん')
+        await ctx.send('にゃーん')
 
 bot.run(token)
